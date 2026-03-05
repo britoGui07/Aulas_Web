eb366c432b013c87dd7c488ff662d7970d79cd44
britoGui07 <andradebritoguilherme@gmail.com>
2026-03-05 17:40:08 -0300

    Resolvido até o exercício 4

--- aula_05_funcao/src/lab_05/lab05.ts
+++ aula_05_funcao/src/lab_05/lab05.ts
@@ -1,6 +1,8 @@
 // Exercício 1
+console.log("\nExercício 1")
+
 function maiuscula(content: string): string{
-    return content.toUpperCase();
+    return content.toUpperCase()
 }
 console.log(maiuscula('jubas'))
 
@@ -8,10 +10,46 @@ const maiusculaLambda = (content: string): string => content.toUpperCase();
 console.log(maiusculaLambda('test'))
 
 // Exercício 2
+console.log("\nExercício 2")
+
 const elevar = (a: number, b: number): number => a ** (b ?? 1);
 console.log(elevar(5, 2))
 
 // Exercício 3
-let numeros: number[] = [12, 5, 28, 7, 9, 37]
+console.log("\nExercício 3")
 
-// const dobrarNumeros = (numeros.filter(n, i => n[i]*2)
+function dobrarNumeros(numeros: number[]): number[]{
+    let dobrados: number[] = []
+    for(const n of numeros){
+        dobrados.push(n*2)
+    }
+    return dobrados
+}
+let numeros: number[] = [12, 5, 28, 7, 9, 37]
+console.log("Array original: ", numeros)
+console.log("Array dobrado: ", dobrarNumeros(numeros))
+
+// Exercício 4
+console.log("\nExercício 4")
+
+type Funcionario ={
+    nome: string
+    cargo: string
+};
+
+const funcionarios: Funcionario[] = [
+    {nome: "Marcos", cargo: "Desenvolvedor"},
+    {nome: "Fernanda", cargo: "Gerente"},
+    {nome: "Carlos", cargo: "Desenvolvedor"},
+    {nome: "Joana", cargo: "Analista"},
+];
+const cargoDesejado: string = "Desenvolvedor"
+
+const funcionarioCargoEspecifico = funcionarios.filter(funcionario=>{ 
+    return funcionario.cargo === cargoDesejado
+})
+console.log(funcionarioCargoEspecifico)
+
+// Exercício 5
+console.log("\nExercício 5")
+
