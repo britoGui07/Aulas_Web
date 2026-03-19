3736d92dbfb03c59d0eab55f6d9839810a3d25c9
britoGui07 <andradebritoguilherme@gmail.com>
2026-03-19 18:18:36 -0300

    Início lab08

--- aula_08_funcaoAssincrona/src/lab/lab08.ts
+++ aula_08_funcaoAssincrona/src/lab/lab08.ts
@@ -1,1 +1,34 @@
-type Tarefa = {descricao: string, prioridade: number, concluida: boolean}
+type Tarefa = {
+    descricao: string, 
+    prioridade: number, 
+    concluida: boolean
+}
+
+const tarefas: Tarefa[] = [
+    { descricao: "Fazer relatório", prioridade: 2, concluida: false},
+    { descricao: "Enviar e-mail", prioridade: 3, concluida: false},
+    { descricao: "Reunião com equipe", prioridade: 1, concluida: false}
+]
+
+function imprimirTarefa(descricao: string, indice: number, totalTarefas: number): void{
+    console.log(`Tarefa concluída: ${descricao}`) ;
+    console.log(`Progresso: ${indice + 1}/${totalTarefas}`) ;
+}
+
+function executarTarefas(
+    tarefas: Tarefa[], 
+    callback: (descricao: string, indice: number, total: number) => void, 
+    intervalo: number = 1000
+): void{
+
+    tarefas.sort((a, b) => a.prioridade - b.prioridade)
+
+}
+/*
+let contador = 0
+const total = tarefas.length
+
+const interval = setInterval(() =>{
+})
+*/
+// executarTarefas(tarefas, )
