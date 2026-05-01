6cc28adc44932825dbc31a8e72b4d347bc6d95fd
britoGui07 <andradebritoguilherme@gmail.com>
2026-04-30 22:13:30 -0300

    Iniciação aula 17/projeto drones

---
+++ aula_17/projeto-drones/src/main.ts
@@ -0,0 +1,9 @@
+const jsonBruto = '{"id_voo": "DRN-7742", "data_envio": "2026-04-28", "detalhes": {"altitude_max": 120, "sensores": ["GPS", "Termometro", "Lidar"]}, "pacotes": [ {"peso": 1.2, "descricao": "Medicamentos"}, {"peso": 0.8, "descricao": "Suprimentos"}] }';
+
+class Main{
+    static executar(){
+        const dadosSimulados = JSON.parse(jsonBruto);
+        console.log("--- Iniciando Sistema de Logistica ---")
+        
+    }
+}
